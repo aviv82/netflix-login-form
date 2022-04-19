278a8420b7fd9f55f5e421464dbeef24abc700db
Aviv <aviv.dror@gmail.com>
2022-04-19 12:20:39 +0200

    netflix setup

---
+++ netflix-login/src/components/SubHeader/SubHeader.jsx
@@ -0,0 +1,11 @@
+import "./SubHeader.css";
+
+export const SubHeader = () => {
+  return (
+    <div className="sub-header">
+      <a className="header-link" href="www.netflix.com">
+        New to Netflix? <span>Sign up Now</span>
+      </a>
+    </div>
+  );
+};
